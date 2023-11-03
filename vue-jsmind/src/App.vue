0ac3ff999276ab50b7e0bab0068bb0c68f098b72
xiahong <807649737@qq.com>
2023-11-03 10:51:30 +0800

    fix

---
+++ vue-jsmind/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <!-- <Demo></Demo> -->
+    <JsMind></JsMind>
+  </div>
+</template>
+
+<script>
+import JsMind from './components/Jsmind.vue'
+// import Demo from './components/Demo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // Demo,
+    JsMind
+  }
+}
+</script>
+
+<style lang="less">
+@import "assets/css/reset.less";
+</style>
